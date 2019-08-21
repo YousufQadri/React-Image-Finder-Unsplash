a2b1a7034ebe8ba66bbda15957c5115bf6a51976
Yousuf <s.yousuf@live.com>
2019-08-21 16:06:42 +0500

    Nav Header Added

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="Navbar-header">
+      <h1>Image Finder</h1>
+    </div>
+  );
+};
+
+export default Navbar;
