c64aa3ec468093d19b32bb775f31f7b97c356efe
Yousuf <s.yousuf@live.com>
2019-08-22 16:10:01 +0500

    Update fetch

--- src/api/unsplashApi.js
+++ src/api/unsplashApi.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization:
-      "Client-ID 37df54ff582bca42c91d85f16011399d5a9214cb45868818989b33513741ff9a"
+    Authorization: "Client-ID YOUR_UNSPLASH_API_KEY_HERE"
   }
 });
